03c1699d09bd01e899242a3fba7488c787ec82fe
Joe Averbukh <joeaverbukh@gmail.com>
2023-11-05 17:49:55 -0500

    Add gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-GD5MXF5JCQ" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-GD5MXF5JCQ');
+        `}
+      </Script>
       <style jsx global>{`
         body {
           background: rgb(255 247 237);
